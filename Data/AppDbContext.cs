51ebf8405d9d3ea203ca9b0eabd520962006a8f4
mariusStefan30 <stefanmariusadrian@yahoo.com>
2025-06-08 13:57:35 +0100

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
